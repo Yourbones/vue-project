82933e8b721a155962f167735c26fc63d95e2284
许浩星 <xuhaoxing9@163.com>
2020-04-26 11:46:34 +0800

    doc: 添加store.js注释

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 全局状态
   state: {
   },
+  // 改变状态的行为
   mutations: {
   },
+  // 异步改变状态的行为
   actions: {
   },
   modules: {
