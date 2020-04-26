138d5d0e6e1ab60a690654fbb46c49b4ac58a0d8
许浩星 <xuhaoxing9@163.com>
2020-04-26 15:24:03 +0800

    feat: 使用axios二次封装http请求方法

---
+++ src/http/axios.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+const baseURL = process.env.API_URL;
+
+const service = axios.create({
+    baseURL: baseURL || 'https://www.baidu.com',
+    // 请求超时时间(ms)
+    timeout: 5000
+});
+
+/* 请求拦截器 */
+service.interceptors.request.use(
+    config => {
+        // 在发送请求之前做些什么
+        return config;
+    },
+    error => {
+        // 对请求错误做些什么
+        return Promise.reject(error);
+    }
+);
+
+/* 响应拦截器 */
+service.interceptors.response.use(
+    response => {
+        // 对响应数据做些什么
+        return response;
+    },
+    error => {
+        // 对响应错误做些什么
+        return Promise.reject(error);
+    }
+);
+
+export default {
+    /* 封装POST方法 */
+    post(url, data = {}) {
+        // 请求发出前可以进行一些处理
+        return service.post(url, data).then(res => {
+            // 对请求返回值进行一些处理
+            return res;
+        }, err => {
+            return Promise.reject(err);
+        });
+    },
+    /* 封装GET方法 */
+    get(url, params) {
+        // 请求发出前做一些处理
+        return service({
+            method: 'get',
+            url: url,
+            params
+        }).then(
+            res => {
+                return res;
+            },
+            err => {
+                return Promise.reject(err);
+            }
+        );
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import http from './http/axios';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$http = http;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
