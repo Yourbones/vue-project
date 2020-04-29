f2031d9e4d20a590784e1fd624462b48dedf3b41
许浩星 <xuhaoxing9@163.com>
2020-04-29 22:37:56 +0800

    feat: 使用axios二次封装http请求

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import http from './http/axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = http
 
 new Vue({
   router,
