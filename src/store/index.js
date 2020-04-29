ee2c45c7844d8e475d9eeaa6779cee64d9de9fbf
许浩星 <xuhaoxing9@163.com>
2020-04-29 22:18:49 +0800

    doc: 添加store.js文件注释

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
+// 该文件用于全局状态管理，等待项目复杂的时候，可以将state,mutations分割出去
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 全局状态
   state: {
   },
+  // 改变状态的行为
   mutations: {
   },
+  // 异步改变状态的行为
   actions: {
   },
+  // 模块
   modules: {
   }
 })
