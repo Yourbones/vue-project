da47ea0121c777705a486ea36a50a530c9e919f3
许浩星 <xuhaoxing9@163.com>
2020-04-29 11:20:30 +0800

    feat: 添加vue.config.js配置文件

---
+++ vue.config.js
@@ -0,0 +1,142 @@
+// vue.config.js
+const path = require('path');
+// 开启gzip压缩， 按需引用
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+// 开启gzip压缩， 按需写入
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
+// 打包分析
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const IS_PROD = ['production'].includes(process.env.NODE_ENV);
+const resolve = (dir) => path.join(__dirname, dir);
+
+module.exports = {
+    // 公共路径
+    publicPath: process.env.NODE_ENV === 'production' ? '/site/vue-demo/' : '/',
+    // 相对于打包路径index.html的路径
+    indexPath: 'index.html',
+    // 'dist', 生产环境构建文件的目录
+    outputDir: process.env.outputDir || 'dist',
+    // 相对于outputDir的静态资源(js、css、img、fonts)目录
+    assetsDir: 'static',
+    // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码
+    lintOnSave: false,
+    // 是否使用包含运行时编译器的 Vue 构建版本
+    runtimeCompiler: true,
+    // 生产环境的 source map
+    productionSourceMap: !IS_PROD,
+    // 是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建。
+    parallel: require('os').cpus().length > 1,
+    // 向 PWA 插件传递选项。
+    pwa: {},
+    chainWebpack: config => {
+        // 修复热更新失效
+        config.resolve.symlinks(true);
+        // 如果使用多页面打包，使用vue inspect --plugins查看html是否在结果数组中
+        config.plugin('html').tap(args => {
+            // 修复 Lazy loading routes Error
+            args[0].chunksSortMode = 'none';
+            return args;
+        });
+        // 添加别名
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('@assets', resolve('src/assets'))
+            .set('@components', resolve('src/components'))
+            .set('@views', resolve('src/views'))
+            .set('@store', resolve('src/store'));
+        // 压缩图片
+        // 需要 npm i -D image-webpack-loader
+        config.module
+            .rule('images')
+            .use('image-webpack-loader')
+            .loader('image-webpack-loader')
+            .options({
+                mozjpeg: { progressive: true, quality: 65 },
+                optipng: { enabled: false },
+                pngquant: { quality: [0.65, 0.9], speed: 4 },
+                gifsicle: { interlaced: false },
+                webp: { quality: 75 }
+            });
+        // 打包分析, 打包之后自动生成一个名叫report.html文件(可忽视)
+        if (IS_PROD) {
+            config.plugin('webpack-report').use(BundleAnalyzerPlugin, [
+                {
+                    analyzerMode: 'static'
+                }
+            ]);
+        }
+    },
+    // cors 相关 https://jakearchibald.com/2017/es-modules-in-browsers/#always-cors
+    // corsUseCredentials: false,
+    // webpack 配置，键值对象时会合并配置，为方法时会改写配置
+    // https://cli.vuejs.org/guide/webpack.html#simple-configuration
+    configureWebpack: config => {
+        // 开启 gzip 压缩
+        // 需要 npm i -D compression-webpack-plugin
+        const plugins = [];
+        if (IS_PROD) {
+            plugins.push(
+                new CompressionWebpackPlugin({
+                    filename: '[path].gz[query]',
+                    algorithm: 'gzip',
+                    test: productionGzipExtensions,
+                    threshold: 10240,
+                    minRatio: 0.8
+                })
+            );
+        }
+        config.plugins = [...config.plugins, ...plugins];
+    },
+    css: {
+        // 是否使用 css 分离插件 ExtractTextPlugin，采用独立样式文件载入，不采用 <style> 方式内联至 html 文件中
+        extract: IS_PROD,
+        // 去掉文件名中的 .module
+        requireModuleExtension: false,
+        // 是否构建样式地图，false 将提高构建速度
+        sourceMap: false,
+        // css预设器配置项
+        loaderOptions: {
+            sass: {
+                prependData: '@import "~@/styles/_var.scss";'
+            }
+        }
+    },
+    devServer: {
+        // 让浏览器 overlay 同时显示警告和错误
+        overlay: {
+            warnings: true,
+            errors: true
+        },
+        // host: 'localhost',
+        // 端口号
+        port: 8080,
+        // https:{type:Boolean}
+        https: false,
+        // 配置自动启动浏览器
+        open: false,
+        hotOnly: true, // 热更新
+        // 配置跨域处理,只有一个代理
+        // proxy: 'http://localhost:8080',
+        // 配置多个跨域
+        proxy: {
+            '/api': {
+                target: 'http://172.11.11.11:7071',
+                changeOrigin: true,
+                // ws: true,//websocket支持
+                secure: false,
+                pathRewrite: {
+                    '^/api': '/'
+                }
+            },
+            '/api2': {
+                target: 'http://172.12.12.12:2018',
+                changeOrigin: true,
+                // ws: true,//websocket支持
+                secure: false,
+                pathRewrite: {
+                    '^/api2': '/'
+                }
+            }
+        }
+    }
+};
