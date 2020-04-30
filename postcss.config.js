3d93cb47712f6ac4dd6ef57f5255aa91b9ab1fc8
许浩星 <xuhaoxing9@163.com>
2020-04-30 09:24:19 +0800

    feat: 添加px转vw设置

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    'postcss-px-to-viewport': {
+      viewportWidth: 375,
+      viewportHeight: 667,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false
+    }
+  }
+}
+
